6553c58ef71666cfc4899e7600510f328f84c15d
Your Name <you@example.com>
2014-01-11 02:42:54 -0500

    javascript optimizations, error-handling, non-miami geolocation handling

--- scripts/userLocation.js
+++ scripts/userLocation.js
@@ -1,51 +1,48 @@
 
-
-function getUserAddress() {
-    
-    var address = $("#inputAddress").val() +", ";
-    var user_location = address + $( "#dropdownCity option:selected" ).text() +" FL";
-
-    document.getElementById("locationInput").innerHTML = 
-                    '<form id ="dynamicForm" method="get" \n\
-                        action="reps_view.html" role="form"><input type="hidden" \n\
-                        id="inputAddress" name="inputAddress" \n\
-                        value="'+user_location+'"></form>';
-        document.getElementById("dynamicForm").submit();
-    
+function createSubmitForm(address) {
+    var str_form = '<form id = "dynamicForm" method = "get" action= "repsView.html" role= "form"><input type= "hidden" id = "inputAddress" name = "inputAddress" value = " ' +address + ' "></form>';;
+    document.getElementById('locationInput').innerHTML = str_form;   
+    document.getElementById('dynamicForm').submit();
 }
 
-
-/*
- * 
- * Make request to Google Maps Geocoder API
- * 
- */
- 
- function geolocateUser() {
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      writeAddressName(pos);
-    });
-  } else {
-    alert('Error: The Geolocation service failed.');
-  }
- }
+function getUserAddress() {
+    var user_location = $("#inputAddress").val() +", " + $("#dropdownCity option:selected").text() + " FL";
+    createSubmitForm(user_location);
+}
 
 function writeAddressName(latLng) {
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({
-          "location": latLng
-    }, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK){
-            var address = results[0].formatted_address;
-            document.getElementById("locationInput").innerHTML = 
-                    '<form id ="dynamicForm" method="get" \n\
-                        action="reps_view.html" role="form"><input type="hidden" \n\
-                        id="inputAddress" name="inputAddress" \n\
-                        value="'+address+'"></form>';
-        document.getElementById("dynamicForm").submit();
-        } else{
-            alert('Location search was unsuccesful. Error : ' + status);
-          } });
- }
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+      "location": latLng
+    },  function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK){
+                var field = results[0];
+                var component = field.address_components;
+                var len = component.length;
+                var addr = 'error';
+                for ( var i = 0; i < len; ++i ) {
+                    if (component[i].long_name === 'Miami-Dade') {
+                        addr = field.formatted_address;
+                        break;
+                    }
+                }
+                createSubmitForm(addr);
+            } 
+            else  
+                alert('Location search was unsuccesful. Error : ' + status);
+        });
+}
+
+/*
+ * Make request to Google Maps Geocoder API
+ */
+function geolocateUser() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            writeAddressName(pos);
+        });
+    } 
+    else  
+        alert('Error: The Geolocation service failed.');
+}
